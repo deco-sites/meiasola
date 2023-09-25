8f675478a192dbc08e443cae01e934b1ac4ff81a
André Martins <andre.lucas.medeir@gmail.com>
2023-09-25 18:07:51 -0300

    feat: WhatsApp

--- components/ui/WhatsApp.tsx
+++ components/ui/WhatsApp.tsx
@@ -1,25 +1,24 @@
 import Icon from "$store/components/ui/Icon.tsx";
 
 export interface Props {
-  phone?: number;
+  phone: number;
 }
 
 function WhatsApp({ phone }: Props) {
-  if (!phone) {
-    return null;
-  }
-
   return (
     <a
       href={`https://api.whatsapp.com/send/?phone=${phone}&text&type=phone_number&app_absent=0`}
-      class="fixed bottom-6 right-6 z-40"
+      class="fixed bottom-4 right-4 z-40"
       aria-label="Chat on WhatsApp"
     >
       <button
-        class="bg-[#45D268] text-white p-2 rounded-full shadow-lg"
         aria-label="Chat on WhatsApp"
+        class="bg-black hover:bg-[#44AD3A] transition-all duration-200 text-white h-[52px] w-[66px] rounded-[24px] flex justify-center items-center"
+        style={{
+          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.15)",
+        }}
       >
-        <Icon id="WhatsApp" size={32} stroke="0.01" />
+        <Icon id="WhatsApp" class="w-8 h-8" />
       </button>
     </a>
   );
