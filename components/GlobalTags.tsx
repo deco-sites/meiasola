5017f2453932eb60ae6b5ce0394b557ac80b1390
André Martins <andre.lucas.medeir@gmail.com>
2023-11-08 07:13:40 -0300

    fix: GTM inside code

--- components/GlobalTags.tsx
+++ components/GlobalTags.tsx
@@ -19,7 +19,8 @@ function GlobalTags() {
         />
 
         <script
-          dangerouslySetInnerHYML={{
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
