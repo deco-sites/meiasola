df1de951f9fd496dd03deba5c37065f39ace2267
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2024-03-13 11:05:39 -0300

    fix nofollow typo

--- components/search/Filters.tsx
+++ components/search/Filters.tsx
@@ -29,7 +29,7 @@ function ValueItem({
   return (
     <a
       href={url}
-      rel="nofollowß"
+      rel="nofollow"
       class={`text-small ${
         selected
           ? "text-black border rounded-full px-3 py-1 inline-flex items-center gap-2 w-fit"
