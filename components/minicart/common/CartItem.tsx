0411485b0b95716fbf53c6dcb14d4f4210a9d551
Sabrina-Btt <sassa.sampaio@gmail.com>
2024-03-26 21:33:58 -0300

    feat(minicart): adicionand link ao produto no carrinho

--- components/minicart/common/CartItem.tsx
+++ components/minicart/common/CartItem.tsx
@@ -8,6 +8,7 @@ import Image from "apps/website/components/Image.tsx";
 import { useCallback, useState } from "preact/hooks";
 
 export interface Item {
+  detailUrl: string
   image: {
     src: string;
     alt: string;
@@ -40,6 +41,7 @@ function CartItem({
   itemToAnalyticsItem,
 }: Props) {
   const {
+    detailUrl,
     image,
     name,
     price: { sale, list },
@@ -47,6 +49,8 @@ function CartItem({
   } = item;
   const isGift = sale < 0.01;
   const [loading, setLoading] = useState(false);
+
+  console.log("url ->", detailUrl)
 
   const withLoading = useCallback(
     <A,>(cb: (args: A) => Promise<void>) =>
@@ -68,7 +72,7 @@ function CartItem({
         gridTemplateColumns: "auto 1fr",
       }}
     >
-      <div class="bg-grey-1 p-1 w-[73px] h-[76px]">
+      <a href={detailUrl} class="bg-grey-1 p-1 w-[73px] h-[76px]">
         <Image
           {...image}
           style={{ aspectRatio: "73 / 76" }}
@@ -76,7 +80,7 @@ function CartItem({
           height={76}
           class="h-full w-full object-cover mix-blend-multiply"
         />
-      </div>
+      </a>
 
       <div class="flex flex-col justify-between pb-1">
         <div class="flex justify-between items-start">
