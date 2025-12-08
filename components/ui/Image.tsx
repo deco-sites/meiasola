1e9ff2edf7ea87531f6933f58ca3d79c33e3a523
guitavano <tavano62@gmail.com>
2025-12-08 10:25:15 -0300

    fix images and bump apps

--- components/ui/Image.tsx
+++ components/ui/Image.tsx
@@ -1,10 +1,18 @@
-import { Head } from "$fresh/runtime.ts";
+import { Head, IS_BROWSER } from "$fresh/runtime.ts";
 import type { JSX } from "preact";
 import { forwardRef } from "preact/compat";
 import { Manifest } from "apps/website/manifest.gen.ts";
 
 export const PATH: `/live/invoke/${keyof Manifest["loaders"]}` =
   "/live/invoke/website/loaders/image.ts";
+const DECO_CACHE_URL = "https://assets.decocache.com/";
+const S3_URL = "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/";
+
+const isAzionAssetsEnabled = () =>
+  IS_BROWSER
+    // deno-lint-ignore no-explicit-any
+    ? (globalThis as any).DECO?.featureFlags?.azionAssets
+    : Deno.env.get("ENABLE_AZION_ASSETS") !== "false";
 
 export type Props =
   & Omit<
@@ -29,25 +37,64 @@ const FACTORS = [1, 2];
 
 type FitOptions = "contain" | "cover";
 
-export const getOptimizedMediaUrl = ({
-  originalSrc,
-  width,
-  height,
-  factor,
-  fit = "cover",
-}: {
+interface OptimizationOptions {
   originalSrc: string;
   width: number;
   height?: number;
   factor: number;
-  fit?: FitOptions;
-}) => {
+  fit: FitOptions;
+}
+
+
+const optimizeVTEX = (opts: OptimizationOptions) => {
+  const { originalSrc, width, height } = opts;
+
+  const src = new URL(originalSrc);
+
+  const [slash, arquivos, ids, rawId, ...rest] = src.pathname.split("/");
+  const [trueId, _w, _h] = rawId.split("-");
+
+  src.pathname = [
+    slash,
+    arquivos,
+    ids,
+    `${trueId}-${width}-${height}`,
+    ...rest,
+  ].join("/");
+
+  return src.href;
+};
+
+
+export const getOptimizedMediaUrl = (opts: OptimizationOptions) => {
+  const { originalSrc, width, height, factor, fit } = opts;
+
+  if (
+    /(vteximg.com.br|vtexassets.com|myvtex.com)\/arquivos\/ids\/\d+/.test(
+      originalSrc,
+    )
+  ) {
+    return optimizeVTEX(opts);
+  }
   const params = new URLSearchParams();
 
-  params.set("src", originalSrc);
   params.set("fit", fit);
   params.set("width", `${Math.trunc(factor * width)}`);
   height && params.set("height", `${Math.trunc(factor * height)}`);
+
+  if (isAzionAssetsEnabled()) {
+    const imageSource = originalSrc
+      .replace(DECO_CACHE_URL, "")
+      .replace(
+        S3_URL,
+        "",
+      )
+      .split("?")[0];
+    // src is being passed separately to avoid URL encoding issues
+    return `https://deco-assets.decoazn.com/image?${params}&src=${imageSource}`;
+  }
+
+  params.set("src", originalSrc);
 
   return `${PATH}?${params}`;
 };
