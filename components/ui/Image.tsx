6e7b44068e74ddef618eb453ecaff431c1bc3add
guitavano <tavano62@gmail.com>
2025-12-11 10:51:15 -0300

    fix image component

--- components/ui/Image.tsx
+++ components/ui/Image.tsx
@@ -1,19 +1,11 @@
-import { Head, IS_BROWSER } from "$fresh/runtime.ts";
+import { Head } from "$fresh/runtime.ts";
 import type { JSX } from "preact";
 import { forwardRef } from "preact/compat";
 import { Manifest } from "apps/website/manifest.gen.ts";
+import { getOptimizedMediaUrl } from "apps/website/components/Image.tsx";
 
 export const PATH: `/live/invoke/${keyof Manifest["loaders"]}` =
   "/live/invoke/website/loaders/image.ts";
-const DECO_CACHE_URL = "https://assets.decocache.com/";
-const S3_URL = "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/";
-
-const isAzionAssetsEnabled = () =>
-  IS_BROWSER
-    // deno-lint-ignore no-explicit-any
-    ? (globalThis as any).DECO?.featureFlags?.azionAssets
-    : Deno.env.get("ENABLE_AZION_ASSETS") !== "false";
-
 export type Props =
   & Omit<
     JSX.IntrinsicElements["img"],
@@ -66,39 +58,6 @@ const optimizeVTEX = (opts: OptimizationOptions) => {
 };
 
 
-export const getOptimizedMediaUrl = (opts: OptimizationOptions) => {
-  const { originalSrc, width, height, factor, fit } = opts;
-
-  if (
-    /(vteximg.com.br|vtexassets.com|myvtex.com)\/arquivos\/ids\/\d+/.test(
-      originalSrc,
-    )
-  ) {
-    return optimizeVTEX(opts);
-  }
-  const params = new URLSearchParams();
-
-  params.set("fit", fit);
-  params.set("width", `${Math.trunc(factor * width)}`);
-  height && params.set("height", `${Math.trunc(factor * height)}`);
-
-  if (isAzionAssetsEnabled()) {
-    const imageSource = originalSrc
-      .replace(DECO_CACHE_URL, "")
-      .replace(
-        S3_URL,
-        "",
-      )
-      .split("?")[0];
-    // src is being passed separately to avoid URL encoding issues
-    return `https://deco-assets.decoazn.com/image?${params}&src=${imageSource}`;
-  }
-
-  params.set("src", originalSrc);
-
-  return `${PATH}?${params}`;
-};
-
 export const getSrcSet = (
   src: string,
   width: number,
@@ -113,7 +72,7 @@ export const getSrcSet = (
           width,
           height,
           factor,
-          fit,
+          fit: fit ?? "cover",
         })
       } ${Math.trunc(factor * width)}w`,
   ).join(", ");
